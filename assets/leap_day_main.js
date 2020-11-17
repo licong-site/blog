5208b150cf4d58c0955774a015233cf1232eaeff
DESKTOP-QGSQME5\licong <licong@starschina.com>
2020-11-17 20:37:36 +0800

    网站搭建

--- assets/leap_day_main.js
+++ assets/leap_day_main.js
@@ -5,15 +5,15 @@
 $(function() {
   $("section h1, section h2, section h3").each(function(){
 
-    var idStr = $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'')
+    var idStr = $(this).text().toLowerCase().replace(/ /g, '-')
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + idStr + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id", idStr);
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
   $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
+    // var position = $($(this).attr("href")).offset().top - 190;
+    // $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
