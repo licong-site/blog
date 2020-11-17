7f0e35805bf9d542b97be3d2bad13ed3120e1f0d
DESKTOP-QGSQME5\licong <licong@starschina.com>
2020-11-17 20:22:54 +0800

    网站搭建

--- assets/leap_day_main.js
+++ assets/leap_day_main.js
@@ -1,22 +1,13 @@
-  
-// var sectionHeight = function() {
-//   var total    = $(window).height(),
-//       $section = $('section').css('height','auto');
-
-//   if ($section.outerHeight(true) < total) {
-//     var margin = $section.outerHeight(true) - $section.height();
-//     $section.height(total - margin - 20);
-//   } else {
-//     $section.css('height','auto');
-//   }
-// }
-
-// $(window).resize(sectionHeight);
+/**
+ * 生成文章目录
+*/
 
 $(function() {
   $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+
+    var idStr = $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'')
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + idStr + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id", idStr);
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
@@ -27,8 +18,4 @@ $(function() {
     $(this).parent().addClass("active");
     event.preventDefault();
   });
-
-  // sectionHeight();
-
-  // $('img').on('load', sectionHeight);
 });
